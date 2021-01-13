c3245bf4172908d2a8b921290cb90992c5f20b20
Nissrine Laalioui <niss@Niss.local>
2021-01-13 14:34:40 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+ session_start();
+ 
+ date_default_timezone_set('Asia/Colombo');
+  
+ if(!isset($_SESSION['expire']))
+ {
+     $_SESSION['expire'] = time()+3600;
+ }
+  
+ $time_remaining = $_SESSION['expire'] - time(); 
+  
+// boucle infinie while(0){echo round(abs($time_remaining) / 60, 2) . ' minutes to go ...';}; 
+?>
+<html>
+    <head>
+        <title>Game name</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <H1>
+            Game title
+        </H1>
+       
+    </body>
+</html>
